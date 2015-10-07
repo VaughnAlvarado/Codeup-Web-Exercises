a1c90f51fd851c363965280a4e2e625baff43809
Vaughn <vaughn@MacPro.local>
2015-10-07 10:32:52 -0500

    Scope stuff

--- public/JS/external.js
+++ public/JS/external.js
@@ -1,4 +1,39 @@
  "use strict";
 
  
-console.log('Hello from external JavaScript!');
+ 
+
+
+
+
+function areaOfRectangle(length, width) {
+       var area = 10 * 5;
+       return area;
+    console.log('test');
+}
+var rectangleArea = areaOfRectangle(10,5);
+console.log(rectangleArea);
+
+var secondArea = areaOfRectangle(30, 3);
+console.log(secondArea);
+
+
+//function isEven(input) {
+	//var remainder = input % 2;
+	//if (remainder === 0) {
+		//return true;
+	//} else {
+	//	return false;
+	//}
+	function isEven(input) {
+	return (input % 2 === 0) ? true : false;
+}
+
+console.log(isEven(30));
+
+
+function sum(a, b) {
+	return a + b;
+}
+
+console.log(sum(100, 500));
